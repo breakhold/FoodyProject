2a459edd305c820e7e9209e9a3e6e58ca2411996
Cihan <cihansimsirrr@gmail.com>
2019-01-22 20:44:05 +0300

    android dependencies added

--- android/app/src/main/java/com/foody/MainApplication.java
+++ android/app/src/main/java/com/foody/MainApplication.java
@@ -3,9 +3,8 @@ package com.foody;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.cmcewen.blurview.BlurViewPackage;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -26,9 +25,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new BlurViewPackage(),
-            new RNGestureHandlerPackage(),
-            new VectorIconsPackage()
+            new VectorIconsPackage(),
+            new RNGestureHandlerPackage()
       );
     }
 
